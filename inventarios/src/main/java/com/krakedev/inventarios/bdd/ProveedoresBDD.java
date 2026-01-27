bada066c7d9ef9d80ca9318739fdfb73663c56c4
HancelE1993 <hancelxyz@gmail.com>
2026-01-27 16:29:34 -0500

    Reto 9 crear proveedores

--- inventarios/src/main/java/com/krakedev/inventarios/bdd/ProveedoresBDD.java
+++ inventarios/src/main/java/com/krakedev/inventarios/bdd/ProveedoresBDD.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+
 import com.krakedev.inventarios.excepciones.KrakeDevException;
 import com.krakedev.inventarios.utils.ConexionBDD;
 import com.krakedev.invetnarios.entidades.Proveedor;
@@ -22,21 +23,24 @@ public class ProveedoresBDD {
 		Proveedor proveedor= null;
 		try {
 			con =ConexionBDD.obtenerConexion();
-			ps=con.prepareStatement("select cedula_p, tipo_documento ,nombre, telefono, correo, direccion "
-					+ "from proveedores "
-					+ "where upper(nombre) like ? ");
+			ps=con.prepareStatement("select prov.cedula_p, prov.tipo_documento,td.descripcion ,prov.nombre, prov.telefono, prov.correo, prov.direccion "
+					+ "from proveedores prov,tipo_documento td "
+					+ "where prov.tipo_documento=td.codigo_tp "
+					+ "and upper(nombre) like ? ");
 			ps.setString(1, "%"+subcadena.toUpperCase()+"%");
 			rs=ps.executeQuery();
 			
 			while(rs.next()) {
 				String cedula_p=rs.getString("cedula_p");
-				String tipo_documento=rs.getString("tipo_documento");
+				String codigoTipoDocumento=rs.getString("tipo_documento");
+				String descripcionTipoDocumento=rs.getString("descripcion");
 				String nombre=rs.getString("nombre");
 				String telefono=rs.getString("telefono");
 				String correo=rs.getString("correo");
 				String direccion=rs.getString("direccion");
+				TipoDocumento td= new TipoDocumento(codigoTipoDocumento,descripcionTipoDocumento);
 				
-				proveedor = new Proveedor(cedula_p,tipo_documento,nombre,telefono,correo,direccion);
+				proveedor = new Proveedor(cedula_p,td,nombre,telefono,correo,direccion);
 				proveedores.add(proveedor);
 			}
 				
@@ -80,6 +84,40 @@ public class ProveedoresBDD {
 		
 		return tipoDocumentos;
 	}
+	
+	public void insertar(Proveedor proveedor) throws KrakeDevException {
+		Connection con = null;
+		PreparedStatement ps=null;
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("insert into proveedores (cedula_p, tipo_documento ,nombre, telefono, correo, direccion) values(?, ?, ?, ?, ?, ?)");
+			ps.setString(1, proveedor.getIdentificador());
+			ps.setString(2, proveedor.getTipoDocumento().getCoodigo());
+			ps.setString(3,proveedor.getNombre());
+			ps.setString(4,proveedor.getTelefono());
+			ps.setString(5,proveedor.getCorreo());
+			ps.setString(6,proveedor.getDireccion());
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeDevException("Error al insertat el cliente. Detalle: "+e.getMessage());
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+
+		}finally {
+			if (con!=null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+
+	}
+
 	
 	
 }
--- inventarios/src/main/java/com/krakedev/invetnarios/entidades/Proveedor.java
+++ inventarios/src/main/java/com/krakedev/invetnarios/entidades/Proveedor.java
@@ -3,7 +3,7 @@ package com.krakedev.invetnarios.entidades;
 public class Proveedor {
 
 	private String identificador;
-	private String tipoDocumento;
+	private TipoDocumento tipoDocumento;
 	private String nombre;
 	private String telefono;
 	private String correo;
@@ -19,7 +19,7 @@ public class Proveedor {
 	public Proveedor() {
 		super();
 	}
-	public Proveedor(String identificador, String tipoDocumento, String nombre, String telefono, String correo,
+	public Proveedor(String identificador, TipoDocumento tipoDocumento, String nombre, String telefono, String correo,
 			String direccion) {
 		super();
 		this.identificador = identificador;
@@ -35,10 +35,10 @@ public class Proveedor {
 	public void setIdentificador(String identificador) {
 		this.identificador = identificador;
 	}
-	public String getTipoDocumento() {
+	public TipoDocumento getTipoDocumento() {
 		return tipoDocumento;
 	}
-	public void setTipoDocumento(String tipoDocumento) {
+	public void setTipoDocumento(TipoDocumento tipoDocumento) {
 		this.tipoDocumento = tipoDocumento;
 	}
 	public String getNombre() {
@@ -65,6 +65,8 @@ public class Proveedor {
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+	
+	
 	
 	
 }
